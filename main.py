b2520d34c6d1366e242087d4cdf2afd73c9667fa
LeopardCheetah <68766450+LeopardCheetah@users.noreply.github.com>
2023-02-16 19:24:34 -0800

    added some things, did some blue allianceing

--- main.py
+++ main.py
@@ -13,16 +13,11 @@ link = 'https://www.thebluealliance.com/api/v3'
 
 
 def getRequest(message, request):
-    print('\n\n'+message+':')
+    print('\n\n'+message)
     response = session.get(link + request)
     return response
 
 
-def printStuff(stuff):
-    # treat this as a str obj
-    print(stuff.text)
-    print('\n\n\n')
-    return
 
 # totally not copied hahahahahhah
 # not too sre what this does, there's something about thread safety but i honestly just use this to send get requests
@@ -33,31 +28,35 @@ session.headers.update({'X-TBA-Auth-Key': input('What is your api key thing?\n')
 get('server status', '/status')
 '''
 
-'''
-print('2022 keys:')
-response = session.get(link + '/events/2022/keys')
-printStuff(response)
 
-print('2023 keys:')
-response = session.get(link + '/events/2023/keys')
-printStuff(response)
-'''
+# 2023 casj => svr 2023
+# 2023 cafr => cvr 2023
+dump = getRequest('', '/event/2023casj/teams/simple')
+dump_svr = dump.text
 
-'''
-response = session.get(link + '/match/2022casj_qm49')
-printStuff(response)
-'''
-
-
-
-dump = getRequest('team codes', '/event/2023casj/teams/simple')
-dump = dump.text
-
-dump_ls = json.loads(dump)
+dump_ls = json.loads(dump_svr)
 # actually a list of dicts
 teams_ls = []
 for team_dict in dump_ls:
     teams_ls.append(int(team_dict['team_number']))
 
 teams_ls.sort()
-print(teams_ls)
+print('teams going to svr (2023):\n', teams_ls, end ='')
+
+
+dump = getRequest('', '/event/2023cafr/teams/simple')
+cvr_teams = json.loads(dump.text)
+cvr_team_ls = []
+
+for p in cvr_teams:
+    cvr_team_ls.append(int(p['team_number']))
+
+cvr_team_ls.sort()
+print('teams going to cvr (2023):')
+print(cvr_team_ls)
+
+print('\n\n')
+
+for i in cvr_team_ls:
+    if i in teams_ls:
+        print(i)
