00eb34fd45a8619cb8d2237ff8868997a908309a
LeopardCheetah <hawkswithmath@gmail.com>
2024-02-27 13:12:08 -0800

    add auton scores + auton correlation (again)

--- 2024_teams.py
+++ 2024_teams.py
@@ -29,22 +29,61 @@ print()
 
 
 
+def get_stats(ls):
+    ls.sort()
+    mean = sum(ls)/len(ls)
+
+    # round the mean
+    mean *= 100
+    mean = int(mean)
+    mean /= 100
+
+
+    mid = -1
+
+    if len(ls) % 2 == 0:
+        mid = (ls[len(ls)//2] + ls[len(ls)//2 - 1])/2
+    else:
+        mid = ls[len(ls)//2]
+
+    min_val = min(ls)
+    max_val = max(ls)
+
+    
+    std_dev = int((sum([(i - mean)**2 for i in ls])/(len(ls) - 1))**0.5)
+
+    # return min, max, median, avg, std.dev
+    return [min_val, max_val, mid, mean, std_dev]
+
+def print_stats(ls, title):
+    r = get_stats(ls)
+    print(title)
+    print(f"Low: {r[0]}, High: {r[1]}, Median: {r[2]}")
+    print(f"Average Value: {r[3]}, Standard Deviation: {r[4]}")
+    print()
+    print()
+    return None
+
+
 
 #################################################################
 
-
-
-dump = getRequest('/event/2024week0/matches')
-bad_match_ids = ['_qm2', '_qm3', 'f1m1', 'f1m2', 'f1m3'] # f1, f2, f3 don't exist on TBA and qm2 and qm3 have incorrect data
+dump = getRequest('/event/2024isde1/matches')
 
 amp_counts = []
 speaker_counts = []
 c = 0
 
-for match in dump:
 
-    if match['key'][-4:] in bad_match_ids:
-        continue
+## Note: 
+## Bottom two parameters are ONLY for section 4 (see below)
+ties = 0
+a = 0
+## 
+
+
+
+for match in dump:
 
     m = match # preserving old code
     s = m["score_breakdown"]
@@ -75,11 +114,32 @@ for match in dump:
     speaker_counts.append([b_sp_a, b_sp_t, b_sp, r_sp_a, r_sp_t, r_sp])
 
 
+
+
+    blue_auto = int(match["score_breakdown"]["blue"]["autoPoints"])
+    red_auto = int(match["score_breakdown"]["red"]["autoPoints"])
+
+    winner = match["winning_alliance"]
+    if winner == 'blue' and blue_auto > red_auto:
+        a += 1
+        continue
+
+    if winner == 'red' and red_auto > blue_auto:
+        a += 1
+        continue
+    
+    if blue_auto == red_auto:
+        ties += 1
+
 # analysis time
 # amp_counts: [blue auto count, blue teleop, red auto, red teleop]
 # speaker_counts: [blue auto, blue teleop (unamplified), blue amplified] + red
 # c total matches
 
+
+
+
+#####################################################################################
 
 
 '''
@@ -97,12 +157,8 @@ for i in range(c):
 
     note_comparison.append(teleop - auto)
 
-note_comparison.sort()
-print(note_comparison)
-print("average teleop - auto note count: ", sum(note_comparison)/c)
-print()
-print()
 
+print_stats(note_comparison, 'Teleop Note Count - Auto Note Count')
 
 
 
@@ -115,11 +171,7 @@ for i in range(c):
 
     amp_v_speaker.append(speaker - amp)
 
-amp_v_speaker.sort()
-print(amp_v_speaker)
-print("avg num of notes scored more in speaker than in amp: ", sum(amp_v_speaker)/c)
-print()
-print()
+print_stats(amp_v_speaker, "Speaker note count - Amp note count")
 
 
 
@@ -130,9 +182,33 @@ total_notes = []
 for i in range(c):
     total_notes.append(sum(amp_counts[i]) + sum(speaker_counts[i]))
 
-total_notes.sort()
-print(total_notes)
-print('avg num of notes/match scored:', sum(total_notes)/c)
-print()
-print()
+print_stats(total_notes, "Average number of notes per match")
+
+
+
+
+# 4. correlation between winning in auto vs winning in the game
+# also count ties!!!
+print("Number of matches:", c)
+print("Number of matches where auto lead won:", a)
+print("Percentage:", str((int((a/c)*10000))/100)+'%') # hehe -- manual rounding
+print("Number of auto ties:", ties)
+
+
+
+
+# 5. Average number of notes in auton
+auton_scored = []
+for i in range(c):
+    auton_scored.append(amp_counts[i][0] + speaker_counts[i][0]) # blue alliance auton
+    auton_scored.append(amp_counts[i][2] + speaker_counts[i][3]) # red alliance auton
+
+print_stats(auton_scored, "Average num of notes scored in autonomous")
+
 '''
+
+
+
+# figure out avg points
+# avg robots climbed per match
+
