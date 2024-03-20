e7786b85c2489fcbbc40be1bbea5a93ce4be208f
LeopardCheetah <hawkswithmath@gmail.com>
2024-03-19 20:16:21 -0700

    added the qm match scout things

--- qm_scouting_list.py
+++ qm_scouting_list.py
@@ -53,6 +53,69 @@ for match in dump:
     # up to here i can basically parse it so that
     # it has the teams in each match in the corresponding lists
     
+    if match['comp_level'] != 'qm':
+        continue
+
+    matches.append([match["match_number"], red_teams, blue_teams])
+
+matches.sort() # now we're in order and back in business
 
 
+def find_team_matches(team_num):
+    # find all their matches
+    ls = []
+
+    for i in range(len(matches)):
+        if team_num in matches[i][1] or team_num in matches[i][2]:
+            ls.append(i+1)
+    
+    return ls
+
+
+to_watch = [[] for _ in range(len(matches) + 1)]
+
+
+
+# ok now let's find teams that we're against :/
+our_matches = find_team_matches(840)
+
+print(our_matches)
+for match_num in our_matches:
+    # find opponents
+    # scrape
+
+    # figure out which side we're on
+    
+    opps = []
+
+    if 840 in matches[match_num-1][1]:
+        opps = matches[match_num-1][2].copy()
+    else:
+        opps = matches[match_num-1][1].copy()
+
+    # ok now find their comp matches 
+
+
+    for opp in opps:
+        ls = find_team_matches(opp)[::-1]
+        
+        for m in range(len(ls)):
+            if ls[m] > match_num:
+                continue
+            
+            if ls[m] == match_num:
+                try:
+                    for j in range(3):
+                        if opp in to_watch[ls[m+1+j]]:
+                            continue
+                        to_watch[ls[m+1+j]].append(opp)
+                
+                except:
+                    break
+            
+            break
+
+for i in range(len(to_watch)):
+    print(i, '\t', to_watch[i])
+            
 
